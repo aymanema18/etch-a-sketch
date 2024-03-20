52fe969ba230d933ff3a9692b3c51f12a70a114a
aymanema18 <aymanema18@gmail.com>
2024-03-20 23:05:32 +0000

    Create the grid and add style to it

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
     let div;
     let container = document.querySelector(".container")
+    let secondContainer;
+    let thirdContainer;
     let divv2;
     for (let i = 0; i < 4; i++) {
-        div = document.createElement("div");
-        div.setAttribute("class", "firstgen");
-        container.appendChild(div);
-        for (let k = 0; k < 4; k++) {
-            divv2 = document.createElement("div");
-            divv2.setAttribute("class", "secondgen");
-            div.appendChild(divv2);
+        secondContainer = document.createElement("div");
+        secondContainer.setAttribute("class", "second-container");
+        container.appendChild(secondContainer);
+        for (let j = 0; j < 4; j++) {
+            div = document.createElement("div");
+            div.setAttribute("class", "firstgen");
+            secondContainer.appendChild(div);
+            for (let c = 0; c < 4; c++) {
+                thirdContainer = document.createElement("div");
+                thirdContainer.setAttribute("class", "third-container");
+                div.appendChild(thirdContainer);
+                for (let k = 0; k < 4; k++) {
+                    divv2 = document.createElement("div");
+                    divv2.setAttribute("class", "secondgen");
+                    thirdContainer.appendChild(divv2);
+                }
+            }
         }
     }
 })
